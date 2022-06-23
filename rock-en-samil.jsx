921798f0724f4e6465100da6b93cee44aa4bfccc
JJ Merelo <jjmerelo@gmail.com>
2022-06-23 08:18:31 +0200

    :see_no_evil: y usa variables

--- rock-en-samil.jsx
+++ rock-en-samil.jsx
@@ -4,29 +4,35 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+import { serve } from "https://deno.land/std@0.144.0/http/server.ts";
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.32/mod.ts";
 
-function App() {
-  return (
-    <html>
-      <head>
-        <title>Hello from JSX</title>
+function App(url) {
+    let pachanga;
+    if ( url == "silbo" ) {
+        pachanga='Pachanga'
+    } else {
+        pachanga='Me lo repita'
+    }
+    return (
+        <html>
+          <head>
+            <title>Qué pasa por la calle</title>
       </head>
-      <body>
-        <h1>Hello world</h1>
-      </body>
-    </html>
-  );
+          <body>
+            <h1>{pachanga}</h1>
+          </body>
+        </html>
+    );
 }
 
 function handler(req) {
-  const html = renderSSR(<App />);
-  return new Response(html, {
-    headers: {
-      "content-type": "text/html",
-    },
-  });
+    const html = renderSSR(<App />, req.url );
+    return new Response(html, {
+        headers: {
+            "content-type": "text/html",
+        },
+    });
 }
 
 serve(handler);
