92a15e2e2d3092e17172ed33b2c0b38808c17a5d
JJ Merelo <jjmerelo@gmail.com>
2022-06-23 08:28:45 +0200

    :poop:

--- rock-en-samil.jsx
+++ rock-en-samil.jsx
@@ -29,7 +29,7 @@ function App(url) {
 
 function handler(req) {
     const thisUrl = new URL(req.url);
-    const html = renderSSR(<App />,  thisUrl.pathname);
+    const html = renderSSR(<App />,  thisUrl.href);
     return new Response(html, {
         headers: {
             "content-type": "text/html",
